f467cd6bb0cb6d8a6cf0d59f357259059778e027
Dmitriy Kuznetsov <dmitriykuznetsow@gmail.com>
2017-04-21 15:24:22 +0700

    Готово все кроме copy_if

--- New2.cpp
+++ New2.cpp
@@ -4,7 +4,9 @@
 #include <algorithm>  
 #include <string>
 using namespace std;
+bool F (const pair<int, char> &x) { return x.first > 5; };
 int main()
+
 {
 	ofstream f;
 	f.open("live.txt");
@@ -13,13 +15,13 @@ int main()
 	map<int, char> map3;
 	int p1 = 0;
 	char p2;
+	int score = 0;
 	int variant = 0;
-	int x = 0;
 	int s = 0;
 	setlocale(LC_ALL, "Russian");
 	while (s == 0)
 	{
-		cout << "Нажмите \n 1. чтобы внести элементы в первый map \n 2. 2-3 \n 3. 4-6 \n 4. 7-8 \n 5. 9-10 \n 6. 11 \n 7. 12 \n 8. выход \n 9. для вывода очередей " << endl;
+		cout << "Нажмите \n 1. чтобы внести элементы в первый map \n 2. 2-3 \n 3. 4-8 \n 4. 9-10 \n 5. 11-12 \n 9. выход \n" << endl;
 		cin >> variant;
 		switch (variant)
 		{
@@ -43,7 +45,7 @@ int main()
 
 		case 3:
 			system("cls");
-			copy_if(map1.begin(), map1.end(), map2.begin(), [](const pair<int, char> &x) { return x.first > 5; });
+			copy_if(map1.begin(), map1.end(), map2.begin(), F);
 			cout << "Первый контейнер " << endl;
 			for (auto it = map1.begin(); it != map1.end(); ++it)
 			{
@@ -55,6 +57,34 @@ int main()
 				cout << it->first << " : " << it->second << endl;
 			}
 			break;
+
+		case 4:
+			system("cls");
+			merge(map1.begin(), map1.end(), map2.begin(), map2.end(),map3.begin());
+			for (auto it = map3.begin(); it != map3.end(); ++it)
+			{
+				cout << it->first << " : " << it->second << endl;
+			}
+			break;
+
+		case 5:
+			system("cls");
+			score = count_if(map3.begin(), map3.end(), F);
+			if (score != 0)
+			{
+				cout << "Соответсвуют " << score << " элементов контейнера" << endl;
+				f << "Соответсвуют " << score << " элементов контейнера" << endl;
+			}
+			else
+			{
+				cout << "В контейнере нет соответсвующих элементов" << endl;
+				f << "В контейнере нет соответсвующих элементов" << endl;
+			}
+			break;
+
+		case 9:
+			system("cls");
+			s = 1;
 
 		default:
 			cout << "Ошибка" << endl;
